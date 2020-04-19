ed64d2a5fac0cedbfc0a1c3c1caf94c0790b2dc6
kewei <x@hanff.cn>
2020-04-19 16:02:04 +0800

    Initial commit

---
+++ rabbitmq-quickstart/src/test/java/com/cvccy/quickstart/RabbitmqQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvccy.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
